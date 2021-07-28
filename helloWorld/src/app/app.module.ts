1e9ca65c90e6e04aa929d85d93675b2c2bb71bb8
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-07-28 13:42:52 -0300

    ngSwitch

--- helloWorld/src/app/app.module.ts
+++ helloWorld/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { ListaLivrosComponent } from './lista-livros/lista-livros.component';
+import { RevistaComponent } from './revista/revista.component';
+import { LivroComponent } from './livro/livro.component';
+import { FilmeComponent } from './filme/filme.component';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    ListaLivrosComponent
+    ListaLivrosComponent,
+    RevistaComponent,
+    LivroComponent,
+    FilmeComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule
