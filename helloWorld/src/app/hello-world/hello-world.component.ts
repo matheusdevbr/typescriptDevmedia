07a942f576a458961feb7f8ac8ed6af57d735493
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-07-31 16:06:19 -0300

    form driven

--- helloWorld/src/app/hello-world/hello-world.component.ts
+++ helloWorld/src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-hello-world',
@@ -32,4 +33,12 @@ export class HelloWorldComponent implements OnInit {
     console.log(event.target.value.length);
   }
 
+  onSubmit(form: NgForm) {
+    let dados = `
+    Nome: ${form.value.nome}
+    Telefone: ${form.value.telefone}
+    Endereço: ${form.value.endereco}`;
+
+    console.log(dados);
+  }
 }
